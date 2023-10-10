2071889965a7f8771e3ff49f4c755a4a762c9840
llocr <36-96@naver.com>
2023-10-10 23:34:53 +0900

    [Init] 프로젝트 초기 생성

---
+++ client-quota/src/pages/_app.js
@@ -0,0 +1,10 @@
+// _app은 서버로 요청이 들어왔을 때 가장 먼저 실행되는 컴포넌트로, 페이지에 적용할 공통 레이아웃의 역할을 합니다.
+// 언제 사용 ? .jsx 파일에서 글로벌하게 React나 Next.js 엘리먼트를 컨트롤하고 싶을 때 쓰면 됩니다.
+// 주요 사용 목적 : 모든 컴포넌트에 공통으로 적용할 속성 관리
+// _app은 로직, 전역 스타일 등 컴포넌트에 공통 데이터를 다룬다. 
+
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ client-quota/src/pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// 프로젝트 생성할 때 딸려온 예시 코드입니다 :)
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}

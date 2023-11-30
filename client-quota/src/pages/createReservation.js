25de43c9bad7d6aa7dcbd29d35811824e6817bdb
cloudmato <cloudmato00@gmail.com>
2023-11-30 15:40:22 +0900

    feat/#4 : 예약 생성 페이지 뷰 생성

---
+++ client-quota/src/pages/createReservation.js
@@ -0,0 +1,7 @@
+import CreateReservationLanding from "@/components/reservation/createReservation/CreateReservationLanding";
+
+const CreateReservationPage = () => {
+    return <CreateReservationLanding/>
+};
+
+export default CreateReservationPage;

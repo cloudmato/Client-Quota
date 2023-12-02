1a0fb7b0c93e266e6931db934bc791626dfbdf12
cloudmato <cloudmato00@gmail.com>
2023-12-02 12:03:11 +0900

    feat(submitReservation.js): 예약폼 페이지 뷰 생성

---
+++ client-quota/src/pages/submitReservation.js
@@ -0,0 +1,7 @@
+import SubmitReservationLanding from "@/components/reservation/submitReservation/SubmitReservationLanding";
+
+const submitReservationPage = () => {
+    return <SubmitReservationLanding/>
+};
+
+export default submitReservationPage;

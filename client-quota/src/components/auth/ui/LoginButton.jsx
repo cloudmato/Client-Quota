e27e93250c3ec30c8a195170241d9911293f09ad
llocr <36-96@naver.com>
2023-12-16 23:54:02 +0900

    modify(auth.js / LoginButton) : 결과보고서 제출을 위한 수정

--- client-quota/src/components/auth/ui/LoginButton.jsx
+++ client-quota/src/components/auth/ui/LoginButton.jsx
@@ -3,28 +3,35 @@ import styled from "styled-components";
 import Image from "next/image";
 import { useGoogleLogin } from "@react-oauth/google";
 import { sendAuthorizationCode } from "@/pages/api/auth";
-import { useAuth } from "@/hooks/useAuth";
+// import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/router";
 
 const LoginButton = () => {
-    const { login } = useAuth();
+    // const { login } = useAuth();
+    const router = useRouter;
 
     const googleLogin = useGoogleLogin({
         flow: 'auth-code',
-        onSuccess: async codeResponse => {
-            try {
-                const response = await sendAuthorizationCode(codeResponse.code);
-                localStorage.setItem('accessToken', response.data.accessToken);
-                localStorage.setItem('user', JSON.stringify(response.data.user));
-                const user= JSON.parse(localStorage.getItem('user'));
-                console.log(user);
-                login(user);
-            } catch (error) {
-                console.error('Error', error);
-            }
+        onSuccess: codeResponse => {
+            console.log(codeResponse)
+            router.push('/main'); //api 연결 안 하고 바로 페이지 이동
         },
-        onError: error => {
-            console.error(error);
-        },
+
+        // onSuccess: async codeResponse => {
+        //     try {
+        //         const response = await sendAuthorizationCode(codeResponse.code);
+        //         localStorage.setItem('accessToken', response.data.accessToken);
+        //         localStorage.setItem('user', JSON.stringify(response.data.user));
+        //         const user= JSON.parse(localStorage.getItem('user'));
+        //         console.log(user);
+        //         login(user);
+        //     } catch (error) {
+        //         console.error('Error', error);
+        //     }
+        // },
+        // onError: error => {
+        //     console.error(error);
+        // },
     });
 
 
--- client-quota/src/pages/api/auth.js
+++ client-quota/src/pages/api/auth.js
@@ -1,9 +1,9 @@
-import axiosInstance from './axiosConfig';
+import axios from "axios";
 
 export const sendAuthorizationCode = (code) => {
     try {
         const registrationId = 'google';
-        const response = axiosInstance.post(`/login/oauth2/code/${registrationId}`, { code });
+        const response = axios.post(`/login/oauth2/code/${registrationId}`, { code });
         return response.data;
     } catch (error) {
         console.error('API 요청 중 오류 발생', error);

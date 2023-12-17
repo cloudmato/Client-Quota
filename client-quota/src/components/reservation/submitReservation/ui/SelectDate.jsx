5eea8b5ee49ad7a93e2cc4980c067cda6441b4c8
cloudmato <cloudmato00@gmail.com>
2023-12-17 21:42:41 +0900

    fix(SelectDate.jsx): 캘린더 컴포넌트 수정 관련 오류 해결

--- client-quota/src/components/reservation/submitReservation/ui/SelectDate.jsx
+++ client-quota/src/components/reservation/submitReservation/ui/SelectDate.jsx
@@ -1,32 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
 import Calendar from '../../../common/Calender';
 
-// SelectDate 컴포넌트: 날짜 선택 UI
 const SelectDate = () => {
-  const [date, setDate] = useState(new Date()); 
+  const [selectedDates, setSelectedDates] = useState([]);
 
-  // 날짜 선택 핸들러
-  const handleSelectDate = (selectedDates) => {
-    if (selectedDates.length > 0) {
-      setDate(selectedDates[selectedDates.length - 1]);
-    }
+  // 예약 데이터 정의
+  const reservationData = {
+    roomName: "first 예약 룸",
+    meetingKind: "직접 만나기",
+    meetingLocation: "이디야",
+    rangeStart: "2023-12-25",
+    rangeEnd: "2023-12-29",
+    durationKind: "HOUR",
+    duration: 1,
+    availableTimeDTOS: [
+      { startTime: "11:00:00", endTime: "12:00:00", wDay: 0 },
+      { startTime: "11:00:00", endTime: "12:00:00", wDay: 1 },
+      { startTime: "11:00:00", endTime: "12:00:00", wDay: 2 },
+      { startTime: "11:00:00", endTime: "12:00:00", wDay: 4 },
+      { startTime: "13:00:00", endTime: "17:00:00", wDay: 5 },
+      { startTime: "13:00:00", endTime: "17:00:00", wDay: 6 },
+    ],
+    excludeDate: ["2023-12-12", "2023-05-09", "2023-05-09"],
+    roomDescription: "room에 대한 설명",
+    roomUrl: "firstRoomUrl",
   };
+
+    // 날짜 선택 핸들러 함수
+    const handleDateSelect = (selectedDates) => {
+      console.log("Dates selected:", selectedDates);
+      setSelectedDates(selectedDates);
+    };
 
   // 제출 핸들러
   const handleSubmit = () => {
-    const selectedDateStr = date.toISOString().split('T')[0];
-    alert(`Selected Date: ${selectedDateStr}`);
+    alert(`Selected Dates: ${selectedDates.map(date => date.toISOString().split('T')[0]).join(', ')}`);
   };
+  
+  
 
   return (
     <SelectDateContainer>
       <DatePickerContainer>
-        <h2>날짜를 선택해주세요</h2> 
-        <Calendar onSelectDate={handleSelectDate} singleSelectMode={true} /> 
+        <h2>날짜를 선택해주세요</h2>
+        <Calendar
+          singleSelectMode={true}
+          onSelectDates={handleDateSelect}
+          availableTime={reservationData.availableTimeDTOS}
+        />
       </DatePickerContainer>
-      <Button onClick={handleSubmit}>확인</Button> 
+      <Button onClick={handleSubmit}>확인</Button>
     </SelectDateContainer>
   );
 };

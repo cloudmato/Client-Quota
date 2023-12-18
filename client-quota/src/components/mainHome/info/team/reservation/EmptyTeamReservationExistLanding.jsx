43fc2cd39938bf107b9fa7b1e3f9d9a085cd471d
jinchiim <nellroll@syuin.ac.kr>
2023-12-19 00:12:28 +0900

    feat(TeamReservationExistLanding.jsx): 팀 유무에 따른 Landing 분기
    -Team 예약이 비어있는 Empty, Reservation 분리

---
+++ client-quota/src/components/mainHome/info/team/reservation/EmptyTeamReservationExistLanding.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const EmptyTeamReservation = () => {
+
+    return (
+        <TeamReservationContainer>
+
+        </TeamReservationContainer>
+    )
+}
+
+export default EmptyTeamReservation;
+
+const TeamReservationContainer = styled.div`
+    background-color: #F1F1F1;
+    margin: 30px;
+    border-radius: 30px;
+    width: 1300px;
+    height: 400px;
+`

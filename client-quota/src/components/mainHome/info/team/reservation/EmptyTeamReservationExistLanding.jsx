b4da2d09c4d383a58fd45afd370c5ad54476caee
jinchiim <nellroll@syuin.ac.kr>
2023-12-20 10:03:55 +0900

    git add~

--- client-quota/src/components/mainHome/info/team/reservation/EmptyTeamReservationExistLanding.jsx
+++ client-quota/src/components/mainHome/info/team/reservation/EmptyTeamReservationExistLanding.jsx
@@ -15,6 +15,6 @@ const TeamReservationContainer = styled.div`
     background-color: #F1F1F1;
     margin: 30px;
     border-radius: 30px;
-    width: 1300px;
+    width: 96%;
     height: 400px;
 `
--- client-quota/src/components/mainHome/info/team/reservation/TeamReservationExistLanding.jsx
+++ client-quota/src/components/mainHome/info/team/reservation/TeamReservationExistLanding.jsx
@@ -19,6 +19,6 @@ const TeamReservationContainer = styled.div`
     background-color: #F1F1F1;
     margin: 30px;
     border-radius: 30px;
-    width: 1300px;
+    width: 96%;
     height: 400px;
 `
